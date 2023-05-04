be15ce64c58ad3944cb96b6157168a585336a956
Joao Madeira <129863118+jodmadeira@users.noreply.github.com>
2023-05-04 17:11:02 +0100

    Lab Done

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 //Method 1 : Using Async Await
 
@@ -19,12 +19,53 @@ const manageRecipes = async () => {
     await Recipe.deleteMany();
 
     // Run your code here, after you have insured that the connection was made
+
+    //Iteration 2
+    let caril = {
+      title: 'Caril',
+      level:'Amateur Chef',
+      ingredients: ['chicken', 'olive oil','Madras powder','coriander', 'tomato paste', 'salt', 'pepper' ],
+      cuisine:'Indian',
+      dishType:'main_course',
+      image:'https://www.pingodoce.pt/wp-content/uploads/2016/09/caril-de-frango.jpeg',
+      duration:90,
+      creator: 'Johnny Woods',
+    }
+
+    await Recipe.create(caril);
+    let foundCaril = await Recipe.find({name: 'Caril'})
+    console.log(foundCaril)
+    
+    
+    //Iteration 3
+    await Recipe.create(data)
+    let allRecipes = await Recipe.find();
+    //console.log(allRecipes)
+    let dishTitles=[]
+    for (let i=0;i<allRecipes.length;i++){
+      dishTitles.push(allRecipes[i].title)
+    }
+    console.log(dishTitles)
+
+    //Iteration 4
+    await Recipe.findOneAndUpdate({title:'Rigatoni alla Genovese'}, {duration:100});
+    //console.log(await Recipe.find({title:'Rigatoni alla Genovese'}));
+    console.log('Sucess! You have updated your dish duration')
+
+    //Iteration 5
+    await Recipe.deleteOne({title:'Carrot Cake'})
+    console.log('You have sucessfully removed Carrot Cake from the Database');
+
+    //Iteration 6
+    mongoose.connection.close(function() {console.log('Mongoose connection closed')});
   } catch (error) {
     console.log(error);
   }
 };
 
 manageRecipes();
+
+
 
 //Method 2: Using .then() method
 //If you want to use this method uncomment the code below:
